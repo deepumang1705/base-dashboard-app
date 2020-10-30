c93a38a64b319b0855a8e2e6a3f0589432af722a
deepumang1705 <deepumang1705@gmail.com>
2020-10-30 12:06:32 +0530

    changed url

--- src/app/pages/forms/register/registration.service.ts
+++ src/app/pages/forms/register/registration.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
 })
 export class RegistrationService {
 
-  _url = environment.baseUrl + '/api/register'
+  _url = environment.baseUrl + '/register'
   constructor(private _http : HttpClient) { }
 
   register(userData) {
